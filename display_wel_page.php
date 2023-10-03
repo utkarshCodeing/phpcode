74d1798e946fb460c7e2b4ae1ec8d597760c0905
utkarshbhardwaj <116480597+utkarshCodeing@users.noreply.github.com>
2023-10-03 19:21:49 +0530

    Add files via upload

---
+++ display_wel_page.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome </title>
+</head>
+<body>
+    <center>
+    <h1>Welcome PHP<h1>
+    <h2> The code for printing welcome on webpage</h2>
+    <center>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
